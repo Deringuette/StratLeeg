60a20c00bea52d86edbfee37ea7c704826178b55
Derek Ringuette <d.ringuette@hotmail.com>
2016-03-07 03:57:30 -0500

    Formatted the create checklist page. Touched: -Site.css -ChecklistScripts.js -Checklist/Create.cshtml -Checklist/_ChecklistGroup.cshtml

--- StrategicLegion/Scripts/ChecklistScripts.js
+++ StrategicLegion/Scripts/ChecklistScripts.js
@@ -1,31 +1,84 @@
-﻿function addChecklistGroup() {
-    var newGroupName = document.getElementById('NewGroupTextbox').value
-
+﻿function addChecklistGroup(checklistGroupNumber) {
+    //Find parent div of checklist
     var Checklist = document.getElementById("NewChecklistDiv");
 
-    var newGroupsDiv = document.createElement('div');
-    newGroupsDiv.setAttribute('class','checklistGroupDiv')
+    //Create div for the new group
+    var newGroupDiv = document.createElement('div');
+    newGroupDiv.setAttribute('class', 'checklistGroupDiv')
+    newGroupDiv.setAttribute('id', 'ChecklistGroupDiv' + checklistGroupNumber)
 
+
+    //Create Div and label to hold header of new group
     var newHeaderDiv = document.createElement('div');
     newHeaderDiv.setAttribute('class', 'checklistHeaderDiv')
 
     var newHeaderLabel = document.createElement('label');
     newHeaderLabel.setAttribute('class', 'checklistHeaderLabel')
+    newHeaderLabel.setAttribute('id', 'ChecklistHeaderLabel' + checklistGroupNumber)
+
+    var headerTextbox = document.getElementById('NewGroupTextbox');
+    var newGroupName = headerTextbox.value;
     newHeaderLabel.innerHTML = newGroupName;
 
-    Checklist.appendChild(newGroupsDiv);
-    newGroupsDiv.appendChild(newHeaderDiv);
+    //Add the group div to the checklist and add the header to the new group
+    Checklist.appendChild(newGroupDiv);
+    newGroupDiv.appendChild(newHeaderDiv);
     newHeaderDiv.appendChild(newHeaderLabel);
 
-    //var newItemPartialDiv = document.createElement('div');
-    //newItemPartialDiv.innerHTML = HTML.renderPartial("_ChecklistItem", Model)
-    //newGroupsDiv.appendChild(newItemPartialDiv);
+    //Create Div which will later hold checklist items for the group
+    var groupItemsDiv = document.createElement('div');
+    groupItemsDiv.setAttribute('class', 'checklistItemsDiv')
+    groupItemsDiv.setAttribute('id', 'ChecklistItemsDiv' + checklistGroupNumber)
+    newGroupDiv.appendChild(groupItemsDiv);
+    
+    //Create Div which will hold the item textbox and add button
+    var newItemTextButtonDiv = document.createElement('div');
+    newItemTextButtonDiv.setAttribute('class', 'newItemTextButtonDiv');
+    newItemTextButtonDiv.setAttribute('id', 'NewItemTextButtonDiv' + checklistGroupNumber);
+    newGroupDiv.appendChild(newItemTextButtonDiv);
+
+    //Create the 'New Group Name Textbox' button
+    var newItemText = document.createElement('input');
+    newItemText.setAttribute('type','text');
+    newItemText.setAttribute('id', 'NewItemTextbox' + checklistGroupNumber);
+    newItemText.setAttribute('class', 'newItemTextbox');
+    newItemText.setAttribute('placeholder','Item Name');
+
+    //Create the 'Add New Group' button
+    var newItemButton = document.createElement('input');
+    newItemButton.setAttribute('type', 'button');
+    newItemButton.setAttribute('id', 'NewItemAddButton' + checklistGroupNumber);
+    newItemButton.setAttribute('value', '+');
+    newItemButton.setAttribute('class', 'newItemAddButton');
+    newItemButton.setAttribute('onclick', 'addChecklistItem(\'' + checklistGroupNumber + '\')');
+
+    //Add the New Group Name Textbox and Button to appropriate div
+    newItemTextButtonDiv.appendChild(newItemText);
+    newItemTextButtonDiv.appendChild(newItemButton);
+
+    //Clear header name textbox for next input
+    headerTextbox.value = '';
 }
 
-function addChecklistItem(parentDiv, elementName) {
-    var innerDiv = document.createElement('div');
-    var elementLabel = document.createElement('label');
-    elementLabel.innerHTML = elementName;
-    innerDiv.appendChild(elementLabel);
-    document.getElementById(parentDiv).appendChild(innerDiv);
+function addChecklistItem(groupNumber) {
+    //Get name of new checklist item and node of appropriate group
+    var itemNameTextbox = document.getElementById('NewItemTextbox' + groupNumber);
+    var newItemName = itemNameTextbox.value;
+
+    //Find div we will add to
+    var groupItemsdiv = document.getElementById('ChecklistItemsDiv' + groupNumber);
+
+    //Create item div and label
+    var itemDiv = document.createElement('div');
+    itemDiv.setAttribute('class', 'checklistItemDiv');
+    var itemLabel = document.createElement('label');
+    itemLabel.setAttribute('class', 'checklistItemLabel');
+    itemLabel.innerHTML = newItemName;
+
+    //Add div and label to form
+    groupItemsdiv.appendChild(itemDiv);
+    itemDiv.appendChild(itemLabel);
+
+    //Clear item name textbox for next input
+    itemNameTextbox.value = '';
 }
